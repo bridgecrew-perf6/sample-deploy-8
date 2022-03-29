79767603cfb43689b3117faf104aaf6815c56a19
Ben Manning <bmanning@me.com>
2022-03-29 15:32:53 -0400

    configure for heroku deployment

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const app = express();
 
 // MIDDLEWARE
 app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/people', async (req, res) => {
   const people = await Person.find()
   res.json(people)
 })
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => {
   console.log('The app is listening')
